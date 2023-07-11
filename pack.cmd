616b4bd530dfbc7da2cc4f70557b360e430ed0e0
waxnet <88239635+waxnet@users.noreply.github.com>
2023-07-12 00:12:27 +0200

    reversed update

--- pack.cmd
+++ pack.cmd
@@ -5,7 +5,6 @@ xcopy /c /y /e . "%userprofile%/Documents/Teardown/mods/MorsWare"
 
 cd "%userprofile%/Documents/Teardown/mods/MorsWare"
 rmdir /s /q .github
-rmdir /s /q media
 del README.md
 del pack.cmd
 del LICENSE
